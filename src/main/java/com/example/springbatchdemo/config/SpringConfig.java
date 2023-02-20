5a6fc8d1d7383d6827c9f05a913cf6f2f1bc513b
Beneko <behzadnekouei@gmail.com>
2023-02-20 12:07:28 +0100

    add dataSource bean

--- src/main/java/com/example/springbatchdemo/config/SpringConfig.java
+++ src/main/java/com/example/springbatchdemo/config/SpringConfig.java
@@ -2,8 +2,12 @@ package com.example.springbatchdemo.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
@@ -15,6 +19,17 @@ public class SpringConfig {
     @Value("classpath:org/springframework/batch/core/schema-mysql.sql")
     private Resource dataRepositorySchema;
 
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/BatchDB?createDatabaseIfNotExist=true" +
+                 "allowPublicKeyRetrieval=true" +
+                "useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
 
 
 }
